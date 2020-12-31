be02b72ddcbc68958fba626e60128c4ce4c55e0f
John Wu <john.wu@skylinetw.com>
2020-12-31 20:40:49 +0800

    refactor: Countdown

--- src/components/countdown.jsx
+++ src/components/countdown.jsx
@@ -1,59 +1,53 @@
 import React, { Component } from "react";
+import Timer from "../libs/utils/timer";
 
 export default class Countdown extends Component {
-  intervalSeconds = 0.1;
 
   constructor(props) {
     super(props);
     this.maximum = parseFloat(props.seconds || 10);
-    this.state = { timer: this.maximum };
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.intervalId);
+    this.timer = new Timer(this.countdown);
+    this.state = { time: this.maximum };
   }
 
   reset = () => {
-    this.pause();
-    this.setState({ timer: this.maximum });
-    this.resume();
+    this.timer.restart();
   }
 
   pause = () => {
-    if (this.intervalId) clearInterval(this.intervalId);
+    this.timer.stop();
   }
 
   resume = () => {
-    this.intervalId = setInterval(this.countdown, this.intervalSeconds * 1000);
+    this.timer.start();
   }
 
   countdown = () => {
-    this.setState(state => ({
-      timer: state.timer - this.intervalSeconds
-    }));
-    this.props.onChanged(this.state.timer);
-    if (this.state.timer > 0) return;
+    this.setState({ time: this.maximum - this.timer.getDuration() });
+    this.props.onChanged(this.state.time);
+    if (this.state.time > 0) return;
     this.timeout();
   }
 
   timeout = () => {
+    this.timer.stop();
     this.props.timeout();
   }
 
   getDuration = () => {
-    return Math.min(this.maximum, this.maximum - this.state.timer);
+    return Math.min(this.maximum, this.timer.getDuration());
   }
 
   getStatus = () => {
-    let rate = Math.round(this.state.timer / this.maximum * 100);
-    if(rate < 20) return "danger";
-    else if(rate < 50) return "warning";
+    let rate = Math.round(this.state.time / this.maximum * 100);
+    if (rate < 20) return "danger";
+    else if (rate < 50) return "warning";
     return "";
   }
 
   render() {
     return (
-      <progress className={this.getStatus()} max={this.maximum} value={this.state.timer.toFixed(1)}></progress>
+      <progress className={this.getStatus()} max={this.maximum} value={this.state.time.toFixed(1)}></progress>
     );
   }
 }
