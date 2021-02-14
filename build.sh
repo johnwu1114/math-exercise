e99ceb418cd717d7184391d362fd15b569adb3aa
John Wu <john.wu@skylinetw.com>
2021-02-15 02:44:58 +0800

    build

--- build.sh
+++ build.sh
@@ -21,7 +21,8 @@ main() {
 exportSitemap() {
     cat << EOF > docs/sitemap.xml
 <?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:xhtml="http://www.w3.org/1999/xhtml">
 EOF
 
     for page in "${PAGES[@]}"; do
